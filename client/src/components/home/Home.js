5cc60cd1dbf9a0c2a8f10e7eb166e79586111893
Harshini0506 <harshinibonagiri06@gmail.com>
2024-06-21 19:54:07 +0530

    First commit

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className='articleHome'>
+      <h1 style={{color:'#191970', margin:'12px'}}> Escape the Ordinary: Unique Travel Experiences</h1>
+      <img src="https://tse2.mm.bing.net/th?id=OIP.XsZ5f_G05gqYsFpSUvuRrgHaE8&pid=Api&P=0&h=180" alt="" className="artcleImage" />
+      <p style={{ fontFamily: 'serif' , paddingTop:'40px'}} className="lead ">
+      Travel blogs serve as a vibrant digital canvas for adventurers to share their experiences, insights, and discoveries from around the globe. These online journals offer readers a glimpse into diverse cultures, breathtaking landscapes, and hidden gems, often accompanied by stunning photography and practical advice. From bustling cityscapes to tranquil nature retreats, travel bloggers provide invaluable tips on accommodations, dining, local customs, and must-see attractions. Their narratives can inspire wanderlust, helping readers plan their own journeys or simply indulge in a bit of armchair travel. Moreover, travel blogs often foster a sense of community, connecting like-minded explorers through shared stories .
+      </p>
+    </div>
+  );
+}
+
+export default Home;
