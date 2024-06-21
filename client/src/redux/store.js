83977aec3645f67d9544df4335e5115bdfc1f7f1
Harshini0506 <164451361+Jyothika215289@users.noreply.github.com>
2024-06-21 19:55:53 +0530

    Revert "First commit"
    This reverts commit 5cc60cd1dbf9a0c2a8f10e7eb166e79586111893.

--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-//create redux store
-import {configureStore} from '@reduxjs/toolkit';
-import userAuthorReducer from './slices/userAuthorSlice';
-
-export const store=configureStore({
-    reducer:{
-        userAuthoruserAuthorLoginReducer:userAuthorReducer
-    }
-})
